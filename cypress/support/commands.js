d526de1694814e584124194554fc81007e345280
Davinder Kumar <Davinder.Kumar@akqa.com>
2020-03-19 12:40:53 +0530

    Test cases

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,9 @@
 //
 // -- This is will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('login', (username, password) => {
+    cy.get('#username').type(username)
+    cy.get('#password').type(password)
+    cy.get('#submit-button').click()
+})
--- cypress/support/index.js
+++ cypress/support/index.js
@@ -15,6 +15,7 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
+import './page_creation'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
